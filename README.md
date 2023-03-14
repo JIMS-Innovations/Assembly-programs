3c4873c77e8cd18041c72fa17db866aa8c27144f
JIMS-Innovations <jimsufficiency@gmail.com>
2023-03-14 02:39:02 +0100

    Initial update

---
+++ README.md
@@ -0,0 +1,5 @@
+Author: Jesutofunmi Kupoluyi
+
+Brief: This is a repository containing assembly programs.
+
+Date: 14 March, 2023  
---
+++ hello_world.asm
@@ -0,0 +1,25 @@
+; @file: hello_world.asm
+; @author: Jesutofunmi Kupoluyi
+; @brief: This is my first assembly program!
+; @date: 14 March, 2023
+
+;Data section
+section .data
+	hello: db "Hello World!", 10 	; String to print
+	helloLen: equ $-hello			; Length of string
+
+;Text section
+section .text
+	global _start					; Entry point for linker
+
+	_start:
+		mov	rax,1					; System write
+		mov rdi,1					; stdout
+		mov rsi,hello				; Message to write
+		mov rdx,helloLen			; Message length
+		syscall						; Call kernel
+
+		; End program
+		mov	rax,60					; system exit
+		mov	rdi,0					; Error code 0 (success)
+		syscall						; Call kernel
